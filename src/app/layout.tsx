b2713f95070a7669aeb0c3322e4579457ae1a14a
Hưng NQH <nguyenquochung16112003@gmail.com>
2025-09-13 06:51:23 +0000

    can you change the main color of the website i want the main color to be

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">{children}</body>
